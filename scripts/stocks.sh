e259890c09c6d5a0b68369e0428ac40cc98a608d
Alex <alexander@artigagonzalez.de>
2018-12-20 14:40:49 +0100

    stock api frequency reduced

--- scripts/stocks.sh
+++ scripts/stocks.sh
@@ -17,7 +17,7 @@ if [ ! -z $symbol ]
 then
   if ! $percent
   then
-    curl -s "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$symbol&apikey=1Q4EHTJC0QOE8OOM" | jq '[."Time Series (Daily)"[]][0]."4. close" | tonumber*100+0.5|floor/100' | awk {'printf "% 6.2f",$1'}
+    curl -s "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$symbol&apikey=YUMJEZ0BTTYO0RX6" | jq '[."Time Series (Daily)"[]][0]."4. close" | tonumber*100+0.5|floor/100' | awk {'printf "% 6.2f",$1'}
   fi
 else
   echo "Missing symbol (-s symbol)..."
@@ -26,6 +26,6 @@ fi
 
 if $percent
 then
-  curl -s "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$symbol&apikey=1Q4EHTJC0QOE8OOM" | jq '.close=([."Time Series (Daily)"[]][0]."4. close" | tonumber)|.start=([."Time Series (Daily)"[]][1]."4. close" | tonumber)|(.close/.start-1)*10000+0.5|floor/100' | awk {'printf "% 5.2f",$1'}
+  curl -s "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$symbol&apikey=YUMJEZ0BTTYO0RX6" | jq '.close=([."Time Series (Daily)"[]][0]."4. close" | tonumber)|.start=([."Time Series (Daily)"[]][1]."4. close" | tonumber)|(.close/.start-1)*10000+0.5|floor/100' | awk {'printf "% 5.2f",$1'}
 fi
   
